8e0bf4764d35118632cc329e2c001bfe5cfd4032
Nitin Misra <nitin@insight.co>
2018-11-24 17:57:38 -0800

    fixed and improved the income dataset parser

--- clean_income_zipcode_data.py
+++ clean_income_zipcode_data.py
@@ -1,111 +1,113 @@
-import csv
-import numpy
-import pandas as pd
+import sys
+
+import pyspark
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster, Session
+from cassandra.query import BatchStatement
+from pyspark.sql import SparkSession, functions, types, DataFrame
+
+assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
+
+spark = SparkSession.builder.appName('CleanIncomeDataset').getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+assert spark.version >= '2.3'  # make sure we have Spark 2.3+
+
+required_states_abbr = ['az', 'pa', 'nv', 'nc', 'oh', 'il']
+states_abbr_mapping = {'arizona': 'az',
+                       'pennsylvania': 'pa',
+                       'nevada': 'nv',
+                       'north carolina': 'nc',
+                       'ohio': 'oh',
+                       'illinois': 'il'}
+
+key_space = 'bigp18'
+query_create_income_table = "CREATE TABLE IF NOT EXISTS income ( zip_code TEXT PRIMARY KEY, " \
+                            "state TEXT, " \
+                            "county TEXT, " \
+                            "combine TEXT, " \
+                            "avg_income INT );"
 
 
-#############################In/out############################
-def open_file(csvfile):
-    reader = pd.read_csv(csvfile)
-    return reader
+def process_income_csv(income_csv_arg):
+    income_df = spark.read.csv(income_csv_arg)
+    income_df = income_df.filter(income_df['_c2'] != 'GEO.display-label') \
+        .select(income_df['_c2'].alias('county_state'), income_df['_c5'].alias('income'))
 
+    county_state_column = pyspark.sql.functions.split(income_df['county_state'], ',')
+    county_name = pyspark.sql.functions.split(county_state_column.getItem(0), ' ').getItem(0)
+    state_name = functions.trim(county_state_column.getItem(1))
 
-def output_file(df, string):
-    df.to_csv(string, index=False)
+    income_df = income_df.withColumn('county', pyspark.sql.functions.lower(county_name)) \
+        .withColumn('state', functions.lower(state_name)) \
+        .withColumn('income', income_df['income'].cast('int')) \
+        .drop('county_state')
 
-
-#############################Clean Data############################
-
-
-#############################helper function#############################
-def clean_zipcode(df):
-    # select information according to each state
-    az = df[df['state'] == 'AZ']
-    pa = df[df['state'] == 'PA']
-    nv = df[df['state'] == 'NV']
-    nc = df[df['state'] == 'NC']
-    il = df[df['state'] == 'IL']
-    oh = df[df['state'] == 'OH']
-    # concatenate together
-    states_df = pd.concat([az, pa, nv, nc, oh, il])
-    # select useful columns only
-    states_df = states_df.iloc[:, [0, 4, 5]]
-    # lower case all letters
-    states_df['state'] = states_df['state'].str.lower()
-    states_df['county'] = states_df['county'].str.lower()
-    # create a column that combine state and county name
-    states_df['combine'] = states_df['county'] + states_df['state']
-    return states_df
-
-
-def clean_income(df):
-    # Select useful columns
-    df = df.iloc[:, [2, 5]]
-    # make two empty list
-    state_list = ['s'] * (df.shape[0])
-    county_list = ['s'] * (df.shape[0])
-    # fill in two lists with county info and state info
-    for i, row in df.iterrows():
-        l = row[0].split(", ")
-        county_list[i] = l[0]
-        state_list[i] = l[1]
-
-    # fill in dataframe columns with lists created aboove
-    df['county'] = county_list
-    df['county'] = df['county'].str.lower()
-    df['state'] = state_list
-    df['state'] = df['state'].str.lower()
-    # Select info according to states
-    AZ = df[df['state'] == 'arizona']
-    PA = df[df['state'] == 'pennsylvania']
-    NV = df[df['state'] == 'nevada']
-    NC = df[df['state'] == 'north carolina']
-    OH = df[df['state'] == 'ohio']
-    IL = df[df['state'] == 'illinois']
-    income_dataframe = pd.concat([AZ, PA, NV, NC, OH, IL])
-    # Replace state names
-    replace = {'arizona': 'az', 'pennsylvania': 'pa', 'nevada': 'nv', 'north carolina': 'nc',
-               'ohio': 'oh', 'illinois': 'il'}
-    income_dataframe.replace(replace, inplace=True)
-
-    # Delete 'county' from county column
-    c_list = []
-    for i, row in income_dataframe.iterrows():
-        c_list.append(row[2].split(" county")[0])
-    income_dataframe['county'] = c_list
-    # Combination column
-    income_dataframe['combine'] = income_dataframe['county'] + income_dataframe['state']
-    # Rename column
-    income_dataframe.rename(columns={'HC02_EST_VC02': 'income'}, inplace=True)
-    # select useful info
-    income_df = income_dataframe.iloc[:, [2, 1, 3, 4]]
+    state_abbr_udf = functions.UserDefinedFunction(lambda state: states_abbr_mapping.get(state), types.StringType())
+    income_df = income_df.withColumn("state_abbr", state_abbr_udf(income_df["state"])).drop('state')
+    income_df = income_df.withColumn('combine', functions.concat(income_df['county'], income_df['state_abbr'])) \
+        .filter(income_df['state_abbr'].isin(required_states_abbr))
     return income_df
 
 
-def clean_income_zip(df1, df2):
-    income_zip = pd.merge(df1, df2, on='combine', how='left')
-    # print(income_zip.head())
+def process_zip_code_state_csv(zip_code_state_csv_arg):
+    zip_code_states_df = spark.read.csv(zip_code_state_csv_arg, header=True)
 
-    income_states = income_zip.dropna()
-    income_states = income_states.iloc[:, [0, 1, 2, 3, 5]]
-    return income_states
-
-
-##############################main######################################
-def main():
-    # open states and income raw data
-    states = open_file("data/zip_codes_states.csv")
-    df = open_file('data/income.csv')
-    # clean states and income data
-    states_df = clean_zipcode(states)
-    income_df = clean_income(df)
-    # print(income_df)
-    # export cleaned income data
-    output_file(income_df, 'data/income_data.csv')
-    # combine states and income data
-    income_states = clean_income_zip(states_df, income_df)
-    print(income_states.shape)
-    output_file(income_states, 'data/income_zipcode.csv')
+    zip_code_states_df = zip_code_states_df \
+        .withColumn('state', functions.lower(zip_code_states_df['state'])) \
+        .withColumn('county', functions.lower(zip_code_states_df['county']))
+    zip_code_states_df = zip_code_states_df.withColumn('combine', functions.concat(zip_code_states_df['county'],
+                                                                                   zip_code_states_df['state']))
+    zip_code_states_df = zip_code_states_df.select('zip_code', 'county', 'state', 'combine') \
+        .filter(zip_code_states_df['state'].isin(required_states_abbr))
+    return zip_code_states_df
 
 
-if __name__ == "__main__":
-    main()
+def merge_income_and_zip_code_data(income_df_arg: DataFrame, zip_code_states_df_arg: DataFrame, session: Session):
+    merged_income_and_zip_code_df = zip_code_states_df_arg.join(income_df_arg,
+                                                                zip_code_states_df_arg['combine'] ==
+                                                                income_df_arg['combine'], how='left') \
+        .select(zip_code_states_df_arg.zip_code,
+                zip_code_states_df_arg.state,
+                zip_code_states_df_arg.county,
+                zip_code_states_df_arg.combine,
+                income_df_arg.income)
+    merged_income_and_zip_code_df.show()
+
+    insert_user_statement = session.prepare(
+        "INSERT INTO income (zip_code, state, county, combine, avg_income) "
+        "VALUES (?, ?, ?, ?, ?)")
+    batch = BatchStatement(consistency_level=ConsistencyLevel.ONE)
+
+    bath_size = 0
+    for row in merged_income_and_zip_code_df.rdd.collect():
+        batch.add(insert_user_statement, (row['zip_code'],
+                                          row['state'],
+                                          row['county'],
+                                          row['combine'],
+                                          row['income'])
+                  )
+        bath_size += 1
+        if bath_size >= 100:
+            session.execute(batch)
+            bath_size = 0
+            batch.clear()
+    session.execute(batch)
+
+    return merged_income_and_zip_code_df
+
+
+def main(income_csv_arg, zip_code_state_csv_arg, merged_csv_path_arg):
+    cluster = Cluster()
+    session = cluster.connect(key_space)
+    session.execute(query_create_income_table)
+
+    income_df = process_income_csv(income_csv_arg)
+    zip_code_state_df = process_zip_code_state_csv(zip_code_state_csv_arg)
+
+    merge_income_and_zip_code_data(income_df, zip_code_state_df, session)
+
+
+if __name__ == '__main__':
+    income_csv = sys.argv[1]
+    zip_code_state_csv = sys.argv[2]
+    main(income_csv, zip_code_state_csv)
